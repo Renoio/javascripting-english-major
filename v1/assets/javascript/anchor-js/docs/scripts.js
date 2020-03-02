acd272f3b1c975a69bb0e03d75f4520ed947bbd1
user <user@comlab-11.molloy.edu>
2020-03-02 11:11:28 -0500

    Tortilla

--- v1/assets/javascript/anchor-js/docs/scripts.js
+++ v1/assets/javascript/anchor-js/docs/scripts.js
@@ -11,8 +11,19 @@
   preEls.filter('.css').find('code').addClass('language-css');
 });*/
 
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
+/*let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a", "b", "c"];
 arrayOfNumbers = [1, 2, 3];
 arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
-$("#response").html(arrayOfStrings[2]);
+$("#response").html(arrayOfStrings[4]);
+*/
+
+let myBurritoObject;
+myBurritoObject = {
+  tortilla: "wheat",
+  guacamole: true,
+  beans:"pinto",
+  habaneroSauceSquirts: 3
+};
+// and let's access a property
+$("#response").html(myBurritoObject["tortilla"]);
